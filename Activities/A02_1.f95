082890f61e2c0ab501835739a4affdb804f4670b
aidkvndfj <ericm7597@gmail.com>
2023-01-18 12:35:25 -0500

    Activity 1 formating changes

--- Activities/A02_1.f95
+++ Activities/A02_1.f95
@@ -1,11 +1,10 @@
 program A02_1
 
-    REAL :: HALF_TIME, TIME, MATERIAL_AT_TIME
-    REAL :: DECAY, INITIAL_MATERIAL, E
+    REAL :: HALF_TIME, TIME, MATERIAL_AT_TIME, DECAY, INITIAL_MATERIAL
+    REAL, PARAMETER :: E = 2.718281828459045
 
     DECAY = 0.0
     INITIAL_MATERIAL = 0.0
-    E = 2.718281828459045
 
     WRITE(*,*) 'Please Input Half Time'
     READ(*,*) HALF_TIME
@@ -14,11 +13,10 @@ program A02_1
     WRITE(*,*) 'Please Input Material at time'
     READ(*,*) MATERIAL_AT_TIME
 
-
     DECAY = log(2.0_4) / HALF_TIME
     INITIAL_MATERIAL = MATERIAL_AT_TIME * E ** (DECAY * TIME)
 
     WRITE(*,100) INITIAL_MATERIAL
-    100 FORMAT('Weight: ', F8.3)
+    100 FORMAT('Initial Weight: ', F8.3)
 
 end
